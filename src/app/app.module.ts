132e9b96aa077985cd5eda6faaa8d23b62c63ff6
HiperEmmanuel <hiperemmanuel@gmail.com>
2018-04-30 13:11:39 -0600

    Routing, Pages y Bootstrap
    Se pasó del HTML5 a Angular5, siguen faltando ciertas cosas todavia.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { OntologiasComponent } from './pages/ontologias/ontologias.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'ontologias', component: OntologiasComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: '**', component: InicioComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    PerfilComponent,
+    RegistroComponent,
+    OntologiasComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/ontologias/ontologias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ontologias',
+  templateUrl: './ontologias.component.html',
+  styles: []
+})
+export class OntologiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
